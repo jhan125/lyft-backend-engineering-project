0991f288acebd325a1aa29f253e44bcf2e25971b
jhan125 <oliviahan125@gmail.com>
2024-01-17 15:00:59 -0800

    Refactor: Implement new class diagram architecture
    - Created battery and engine classes as per the updated class diagram.
    - Refactored existing code to integrate new class hierarchy.
    - Updated functions/methods to align with the new architecture.
    - Removed deprecated classes and functions no longer used.

---
+++ batteries/spindler_battery.py
@@ -0,0 +1,15 @@
+from battery import Battery
+from datetime import datetime
+
+class SpindlerBattery(Battery):
+    def __init_(self, last_service_date):
+        super().__init__(self, last_service_date)
+        self.last_service_date = last_service_date
+   
+    def battery_needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        
+        if service_threshold_date < datetime.today().date():
+            return True
+        else:
+            return False
---
+++ car.py
@@ -0,0 +1,16 @@
+from battery import Battery
+from engines import Engine
+
+class Car(Engine, Battery):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
+
+    def battery_needs_service(self):
+        return self.battery.battery_needs_service()
+
+    def engine_needs_service(self):
+        return self.engine.engine_needs_service()
+
+    def needs_service(self):
+        return self.engine_needs_service() or self.battery_needs_service() or self.tire_needs_service()
